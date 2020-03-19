44091b776d722e5f5d5d75f070e8c70b30d8d908
Quentin <Quentin.lecocq@yahoo.com>
2020-03-19 13:32:58 +0100

    add: add some style to my navbar for she is responsive and now work in mobile, desktop !

--- src/components/Layout/header.js
+++ src/components/Layout/header.js
@@ -1,54 +1,75 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
-import Logo from "../../images/logo.png"
+import React, { Component } from "react"
+import Logo from "../../images/logo-red.png"
 import { FaBars, FaBookOpen } from "react-icons/fa"
 
-const Header = () => (
-  <header>
-    <nav>
-      <ul className="menu">
-        <li className="logo">
-          <Link to="#">
-            <img src={Logo} alt="" />
-          </Link>
-        </li>
-        <li className="item">
-          <Link to="#">Shop</Link>
-        </li>
-        <li className="item">
-          <Link to="#">Gift Cards</Link>
-        </li>
-        <li className="item">
-          <Link to="#">Food Track</Link>
-        </li>
-        <li className="item">
-          <Link to="#">Email Club</Link>
-        </li>
-        <li className="item">
-          <Link to="#">Contact</Link>
-        </li>
-        <span></span>
-        <li className="menu-btn">
-          <Link to="#">Menu</Link>
-          <FaBookOpen />
-        </li>
-        {/* <li className="toggle">
-          <Link to="#">
-            <FaBars />
-          </Link>
-        </li> */}
-      </ul>
-    </nav>
-  </header>
-)
+class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false,
+    }
+  }
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  toggleActive = () => {
+    this.setState(prevState => ({ active: !prevState.active }))
 
-Header.defaultProps = {
-  siteTitle: ``,
+    const navLinks = document.querySelectorAll(".nav-links li")
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ""
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+          0.7}s`
+      }
+    })
+  }
+
+  render() {
+    return (
+      <header>
+        <nav>
+          <div className="logo">
+            <Link to="#">
+              <img src={Logo} alt="" />
+            </Link>
+          </div>
+          <ul className={`nav-links ${this.state.active ? "nav-active" : ""}`}>
+            <li>
+              <Link to="#">Shop</Link>
+            </li>
+            <li>
+              <Link to="#">Gift Cards</Link>
+            </li>
+            <li>
+              <Link to="#">Food Track</Link>
+            </li>
+            <li>
+              <Link to="#">Email Club</Link>
+            </li>
+            <li>
+              <Link to="#">Contact</Link>
+            </li>
+            <li>
+              <Link to="#" className="menu">
+                Menu
+              </Link>
+              {/* <FaBookOpen /> */}
+            </li>
+          </ul>
+          <div
+            className={`burger ${this.state.active ? "toggle" : ""}`}
+            onClick={this.toggleActive}
+          >
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
+        </nav>
+      </header>
+    )
+  }
 }
 
 export default Header

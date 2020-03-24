61062412e292a8bb8bed9cc2c4b6b95c5f71636e
Quentin <Quentin.lecocq@yahoo.com>
2020-03-24 11:05:27 +0100

    add: add new component footer and style, add some links to my social media. Not responsive yet

---
+++ src/components/Layout/footer.js
@@ -0,0 +1,84 @@
+import React from "react"
+import {
+  AiOutlineInstagram,
+  AiOutlineGithub,
+  AiOutlineMedium,
+  AiOutlineFacebook,
+  AiOutlineLinkedin,
+} from "react-icons/ai"
+
+const footer = () => {
+  return (
+    <div className="footer-section">
+      <div className="footer-left">
+        <p>
+          Copyright &copy; {new Date().getFullYear()} by{" "}
+          <span>GoodBurger.</span> All Rights Reserved.
+        </p>
+      </div>
+      <div className="footer-right">
+        <p>
+          Website by{" "}
+          <span>
+            <a
+              href="https://www.quentin-lecocq.fr"
+              target="_blank"
+              rel="noopener norefferer"
+            >
+              Quentin Lecocq
+            </a>
+          </span>
+        </p>
+        <ul className="footer-social-media">
+          <li>
+            <a
+              href="https://www.facebook.com/quentin.lecocq.142"
+              target="_blank"
+              rel="noopener norefferer"
+            >
+              <AiOutlineFacebook />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/quentiinlecocq/"
+              target="_blank"
+              rel="noopener norefferer"
+            >
+              <AiOutlineInstagram />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/quentin-lecocq-47b354160/"
+              target="_blank"
+              rel="noopener norefferer"
+            >
+              <AiOutlineLinkedin />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/Quentin0292"
+              target="_blank"
+              rel="noopener norefferer"
+            >
+              <AiOutlineGithub />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://medium.com/@quentinlecocq"
+              target="_blank"
+              rel="noopener norefferer"
+            >
+              <AiOutlineMedium />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default footer
